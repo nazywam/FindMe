cb8a44be2c8bb409a0ebe506829fd45ae63954b0
unknown <nazywam@gmail.com>
2016-02-27 22:54:08 +0100

    Custom parsing

--- app/src/main/java/findme/findme/MapParser.java
+++ app/src/main/java/findme/findme/MapParser.java
@@ -3,7 +3,14 @@ package findme.findme;
 import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.kml.KmlContainer;
+import com.google.maps.android.kml.KmlGeometry;
 import com.google.maps.android.kml.KmlLayer;
+import com.google.maps.android.kml.KmlPlacemark;
+import com.google.maps.android.kml.KmlPoint;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -22,7 +29,24 @@ public class MapParser {
 
         InputStream iStream = context.getAssets().open("Example.kml");
         KmlLayer layer = new KmlLayer(mMap, iStream, context);
-        layer.addLayerToMap();
+
+        for(KmlContainer container : layer.getContainers()){
+//            Log.d("Container", " "+container.getProperty("name"));
+            for(KmlContainer c : container.getContainers()){
+                for(KmlPlacemark placemark : c.getPlacemarks()){
+                    //KmlGeometry geom = placemark.getGeometry();
+
+                    String geomS = placemark.getGeometry().getGeometryObject().toString();
+                    String loc = geomS.substring(geomS.indexOf("(") + 1, geomS.indexOf(")"));
+
+                    Float lat = Float.parseFloat(loc.split(",")[0]);
+                    Float lng = Float.parseFloat(loc.split(",")[1]);
+
+                    MarkerOptions m = new MarkerOptions().position(new LatLng(lat, lng)).title(placemark.getProperty("name"));
+                    mMap.addMarker(m);
+                }
+            }
+        }
     }
 
 }
