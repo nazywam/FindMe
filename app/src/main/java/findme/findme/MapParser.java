a0b38ba5936e72dff6c1423dad2d48681ded091e
unknown <nazywam@gmail.com>
2016-02-27 20:49:12 +0100

    MapParser init

---
+++ app/src/main/java/findme/findme/MapParser.java
@@ -0,0 +1,15 @@
+package findme.findme;
+
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+
+/**
+ * Created by Michał on 2016-02-27.
+ */
+public class MapParser {
+    private Kml kml;
+
+    public MapParser(){
+
+    }
+
+}
